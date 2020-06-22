4afd2a317e55ace26c99121527875399fffccf6f
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-06-22 16:07:37 -0400

    Create helloworld.sh

---
+++ week1/helloworld.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -p shared
+#SBATCH -N 1
+#SBATCH -n 1 
+#SBATCH --mem 1000
+#SBATCH -t 0-30:00
+#SBATCH -o slurm.%N.%j.out
+#SBATCH -e slurm.%N.%j.err
+
+# submit this script from your home directory 
+
+echo "hello world" > log.out

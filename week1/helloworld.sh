0505ba380566ccc25e6d2b5baacf302441f76a07
Tim Sackton <tsackton@g.harvard.edu>
2020-06-23 10:59:05 -0400

    Small updates to part1 slurm script

--- week1/helloworld.sh
+++ week1/helloworld.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH -p shared
 #SBATCH -N 1
-#SBATCH -n 1 
+#SBATCH -n 1
 #SBATCH --mem 1000
-#SBATCH -t 0-30:00
+#SBATCH -t 30
 #SBATCH -o slurm.%N.%j.out
 #SBATCH -e slurm.%N.%j.err
 
-# submit this script from your home directory by typing sbatch helloworld.sh 
+# submit this script from your home directory by typing sbatch helloworld.sh
 
 touch hello_world.txt

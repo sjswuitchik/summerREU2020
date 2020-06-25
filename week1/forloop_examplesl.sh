b229e49cb44e5d6d342d44b11775a092491ed565
Sara JS Wuitchik <sjswuit@g.harvard.edu>
2020-06-25 14:29:45 -0400

    Create forloop_examplesl.sh

---
+++ week1/forloop_examplesl.sh
@@ -0,0 +1,22 @@
+## try running these yourself to see how each of functions work
+
+# example 1
+
+for i in 1 2 3 4 5
+do
+   echo "Hello $i times"
+done
+
+# ex 2
+
+for i in {1..5}
+do
+  echo "Hello $i times"
+done
+
+# ex 3
+
+for i in {0..10..2}
+  do 
+     echo "Hello $i times"
+ done
